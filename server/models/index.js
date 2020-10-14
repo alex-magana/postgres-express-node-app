a25f8eeba9f302e4f69578de7d7ae469bc2ba278
alex-magana <alex.magana@andela.com>
2020-10-14 06:43:41 +0300

    Add sequelize files

---
+++ server/models/index.js
@@ -0,0 +1,45 @@
+// In this file, we are requiring the modules we're going to be using.
+// Then, we're reading the configuration specific to our current Node 
+// environment. If we don't have a Node environment defined, we're
+// defaulting to development. Then, we are establishing a connection
+// with our database, after which we read our models folder, discovering
+// and importing any and all the models in it, adding them to the db object
+// and applying relationships between the models, if such relationships exist.
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
