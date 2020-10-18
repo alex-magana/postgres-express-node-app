13f090bf2523f5e0bcf83508fd6775d2d98cef39
alex-magana <alex.magana@andela.com>
2020-10-18 17:56:53 +0300

    List all Todos

--- server/routes/todos/index.js
+++ server/routes/todos/index.js
@@ -2,4 +2,5 @@ const todosController = require('../../controllers').todos;
 
 module.exports = (app) => {
     app.post('/api/todos', todosController.create);
+    app.get('/api/todos', todosController.list);
 }
