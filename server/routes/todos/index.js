85c9181bd0b83ef942fe2bf1f772862c602d5f38
alex-magana <alex.magana@andela.com>
2020-10-19 07:14:40 +0300

    Add route to update Todo record

--- server/routes/todos/index.js
+++ server/routes/todos/index.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
     app.post('/api/todos', todosController.create);
     app.get('/api/todos', todosController.list);
     app.get('/api/todos/:todoId', todosController.retrieve);
+    app.put('/api/todos/:todoId', todosController.update);
 }
