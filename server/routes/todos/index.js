5214f0623f9277024a693dc20d8c069791ecb11c
alex-magana <alex.magana@andela.com>
2020-10-19 06:50:05 +0300

    Retrieve Todo by id

--- server/routes/todos/index.js
+++ server/routes/todos/index.js
@@ -3,4 +3,5 @@ const todosController = require('../../controllers').todos;
 module.exports = (app) => {
     app.post('/api/todos', todosController.create);
     app.get('/api/todos', todosController.list);
+    app.get('/api/todos/:todoId', todosController.retrieve);
 }
